de96fb0e370d7bd34b1496f216beaaf1960a59ba
Karungg <miftahfadilah71@gmail.com>
2024-06-07 09:18:34 +0700

    Move login button to sidebar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,17 +8,24 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->group('admin', ['filter' => 'role:admin'], static function ($routes) {
+    // Route Dashboard
     $routes->get('', 'DashboardController::index');
+
     // Route packages
-    $routes->get('packages', 'PackageController::index');
-    $routes->get('packages/create', 'PackageController::create');
-    $routes->post('packages/create', 'PackageController::store');
-    $routes->get('packages/(:num)/edit', 'PackageController::edit/$1');
-    $routes->put('packages/(:num)/edit', 'PackageController::update');
-    $routes->delete('packages/delete/(:num)', 'PackageController::destroy/$1');
+    $routes->group('packages', static function ($routes) {
+        $routes->get('', 'PackageController::index');
+        $routes->get('create', 'PackageController::create');
+        $routes->post('create', 'PackageController::store');
+        $routes->get('(:num)/edit', 'PackageController::edit/$1');
+        $routes->put('(:num)/edit', 'PackageController::update');
+        $routes->delete('delete/(:num)', 'PackageController::destroy/$1');
+    });
+
     // Route memberships
-    $routes->get('memberships', 'MembershipController::index');
-    $routes->get('memberships/(:num)/edit', 'MembershipController::edit/$1');
-    $routes->put('memberships/(:num)/edit', 'MembershipController::update');
-    $routes->delete('memberships/delete/(:num)', 'MembershipController::destroy/$1');
+    $routes->group('memberships', static function ($routes) {
+        $routes->get('', 'MembershipController::index');
+        $routes->get('(:num)/edit', 'MembershipController::edit/$1');
+        $routes->put('(:num)/edit', 'MembershipController::update');
+        $routes->delete('delete/(:num)', 'MembershipController::destroy/$1');
+    });
 });
